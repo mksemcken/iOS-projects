f9e1c62b70f7357883821f8a0f4f534b5f018a40
Matt Semcken <MattSemcken@rgnt2-81-251-dhcp.int.colorado.edu>
2013-10-03 10:53:35 -0600

    Picker

--- Picker/Picker/ViewController.h
+++ Picker/Picker/ViewController.h
@@ -14,6 +14,9 @@
 @property (weak, nonatomic) IBOutlet UIPickerView *musicPicker;
 @property (weak, nonatomic) IBOutlet UILabel *choiceLabel;
 @property (strong, nonatomic) NSArray *genre;
+@property (strong, nonatomic) NSArray *decade;
+
+
 
 
 @end
--- Picker/Picker/ViewController.m
+++ Picker/Picker/ViewController.m
@@ -19,22 +19,37 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     NSArray *array=[[NSArray alloc] initWithObjects:@"Country", @"Pop", @"Rock", @"Alternative", @"Hip Hop", @"Jazz", @"Classical", nil];
+    NSArray *array2=[[NSArray alloc] initWithObjects:@"1950s", @"1960s", @"1970s", @"1980s", @"1990s", @"2000s", @"2010s", nil];
+    
     _genre=array;
+    _decade=array2;
 }
 
 //Methods Required to Implement Picker
--(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{return 1; }//number of components
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{return 2; }//number of components
     
 //Required for DATASOURCE PROTOCAL
-    -(NSInteger)pickerView: (UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {return [_genre count];}
+-(NSInteger)pickerView: (UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+        {if(component==0)
+                return [_genre count];
+                else return [_decade count];
+        }
 //Picker Delegate Methods
     -(NSString *)pickerView:(UIPickerView *)pickerView
                  titleForRow:(NSInteger) row
                 forComponent:(NSInteger) component {
-                    return [_genre objectAtIndex:row];}
+        
+        if (component==0)
+                    return [_genre objectAtIndex:row];
+        else return [_decade objectAtIndex:row];
+    }
     
 //row is selected
--(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{_choiceLabel.text = [NSString stringWithFormat:@"You Like %@", [_genre objectAtIndex:row]];}
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+    NSInteger genrerow=[_musicPicker selectedRowInComponent:0];
+    NSInteger decaderow=[_musicPicker selectedRowInComponent:1];
+    
+    _choiceLabel.text = [NSString stringWithFormat:@"You Like %@ from the %@", [_genre objectAtIndex:genrerow], [_decade objectAtIndex:decaderow]];}
 
 - (void)didReceiveMemoryWarning
 {
