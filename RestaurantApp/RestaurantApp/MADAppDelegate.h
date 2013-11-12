c948608be59de60a9eda77d3a4e72dd8aefbe395
Matt Semcken <MattSemcken@rgnt2-82-94-dhcp.int.colorado.edu>
2013-11-12 09:10:01 -0700

    project 2

---
+++ RestaurantApp/RestaurantApp/MADAppDelegate.h
@@ -0,0 +1,12 @@
+//
+//  MADAppDelegate.h
+//  RestaurantApp
+//  Copyright (c) 2013 Matt Semcken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@end
---
+++ RestaurantApp/RestaurantApp/MADFirstViewController.h
@@ -0,0 +1,12 @@
+//
+//  MADFirstViewController.h
+//  RestaurantApp
+//  Copyright (c) 2013 Matt Semcken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADFirstViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
+
+
+@end
