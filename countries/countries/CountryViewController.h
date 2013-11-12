71de18ddae1bf917f77666346269ffe5e2d2db9c
Matt Semcken <MattSemcken@rgnt2-82-94-dhcp.int.colorado.edu>
2013-11-12 09:11:45 -0700

    countries

---
+++ countries/countries/CountryViewController.h
@@ -0,0 +1,14 @@
+//
+//  CountryViewController.h
+//  countries
+//
+//  Created by Matthew Semcken on 11/5/13.
+//  Copyright (c) 2013 MKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CountryViewController : UIViewController
+
+
+
---
+++ countries/countries/CountryViewController.m
@@ -0,0 +1,38 @@
+//
+//  CountryViewController.m
+//  countries
+//
+//  Created by Matthew Semcken on 11/5/13.
+//  Copyright (c) 2013 MKS. All rights reserved.
+//
+
+#import "CountryViewController.h"
+
+@interface CountryViewController ()
+
+@end
+
+@implementation CountryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
