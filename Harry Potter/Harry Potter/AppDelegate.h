71f0800b9b62156c271e7004b9bb7aafad8e8df9
Matt Semcken <MattSemcken@econ1-144-187-dhcp.int.colorado.edu>
2013-11-11 19:00:44 -0700

    HP

---
+++ Harry Potter/Harry Potter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Harry Potter
+//
+//  Created by Matthew Semcken on 11/4/13.
+//  Copyright (c) 2013 MKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Harry Potter/Harry PotterTests/Harry_PotterTests.m
@@ -0,0 +1,34 @@
+//
+//  Harry_PotterTests.m
+//  Harry PotterTests
+//
+//  Created by Matthew Semcken on 11/4/13.
+//  Copyright (c) 2013 MKS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Harry_PotterTests : XCTestCase
+
+@end
+
+@implementation Harry_PotterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
